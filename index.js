9a07f6d04f3cd226f98a0548cc80536ff15d5fd9
Ifeoluwa <olabodeifeoluwa712@gmail.com>
2026-03-03 10:56:26 +0100

    .

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ trigger2.addEventListener("click", () => {
     console.log("The interest is", interest)
     const total_amount = Number(principal.value)  + interest;
     console.log("The totalamount is",total_amount)
-    const summary =("After ",period.value," years at an interest rate of",rate.value ,"the total amount will be ", total_amount)
+    const summary =("After " + period.value +" years at an interest rate of" + rate.value +"the total amount will be " + total_amount)
     document.getElementById("result").textContent = summary ;
+    // document.write(summary);
 });
